134d71a176444032accb6789636998af96d139fa
mmazitov <mazitov.nikolya@gmail.com>
2024-10-28 16:40:17 +0200

    initial comit

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,131 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
-const sass = require('gulp-sass')(require('sass'));
-const eslint = require('gulp-eslint');
-const concat = require('gulp-concat');
-const cleanCSS = require('gulp-clean-css');
-const terser = require('gulp-terser');
-const babel = require('gulp-babel');
-const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass')(require('sass')); // Import Sass compiler
+const eslint = require('gulp-eslint'); // ESLint for JavaScript linting
+const concat = require('gulp-concat'); // Concatenate files
+const cleanCSS = require('gulp-clean-css'); // Minify CSS
+const terser = require('gulp-terser'); // Minify JavaScript
+const browserSync = require('browser-sync').create(); // Live reloading server
 
-// Функция для динамического импорта gulp-imagemin
-async function imagemin() {
-	const imagemin = await import('gulp-imagemin');
-	return imagemin.default;
-}
-
-// Пути к файлам
+// Paths to files
 const paths = {
 	pug: {
-		src: './src/pug/**/*.pug',
-		dist: 'dist',
-		pages: './src/pug/pages/**/*.pug',
+		src: './src/pug/**/*.pug', // Source Pug files
+		dist: 'dist', // Destination for compiled HTML
+		pages: './src/pug/pages/**/*.pug', // Source for Pug pages
 	},
 	styles: {
-		src: './src/scss/**/*.scss',
-		dist: './dist/css',
+		src: './src/scss/**/*.scss', // Source SCSS files
+		dist: './dist/css', // Destination for compiled CSS
 	},
 	scripts: {
-		src: './src/js/**/*.js',
-		dist: './dist/js',
+		src: './src/js/**/*.js', // Source JavaScript files
+		dist: './dist/js', // Destination for compiled JavaScript
 	},
 	fonts: {
-		src: './src/fonts/**/*',
-		dist: './dist/fonts',
+		src: './src/fonts/**/*', // Source font files
+		dist: './dist/fonts', // Destination for fonts
 	},
 	images: {
-		src: './src/images/**/*',
-		dist: './dist/images',
+		src: './src/images/**/*', // Source image files
+		dist: './dist/images', // Destination for images
 	},
 	pic: {
-		src: './src/pic/**/*',
-		dist: './dist/pic',
+		src: './src/pic/**/*', // Source additional image files
+		dist: './dist/pic', // Destination for additional images
 	},
 };
 
-// Компиляция Pug в HTML
+// Task to compile Pug into HTML
 function pugTask() {
 	return gulp
-		.src(paths.pug.pages)
-		.pipe(
-			pug({
-				pretty: true,
-			})
-		)
-		.pipe(gulp.dest(paths.pug.dist))
-		.pipe(browserSync.stream());
+		.src(paths.pug.pages) // Get Pug page files
+		.pipe(pug({ pretty: true })) // Compile to HTML with pretty formatting
+		.pipe(gulp.dest(paths.pug.dist)) // Save compiled HTML to destination
+		.pipe(browserSync.stream()); // Inject changes into the browser
 }
 
-// Компиляция SCSS в CSS
+// Task to compile SCSS into CSS
 function sassTask() {
 	return gulp
-		.src(paths.styles.src)
-		.pipe(sass().on('error', sass.logError)) // Обработка ошибок
-		.pipe(cleanCSS()) // Минификация CSS
-		.pipe(gulp.dest(paths.styles.dist)) // Сохранение CSS в dist
-		.pipe(browserSync.stream()); // Перезагрузка браузера
+		.src(paths.styles.src) // Get SCSS files
+		.pipe(sass().on('error', sass.logError)) // Compile SCSS and handle errors
+		.pipe(cleanCSS()) // Minify the compiled CSS
+		.pipe(gulp.dest(paths.styles.dist)) // Save CSS to destination
+		.pipe(browserSync.stream()); // Inject changes into the browser
 }
 
-// Линтинг и компиляция JavaScript
+// Task for linting and compiling JavaScript
 function jsTask() {
 	return gulp
-		.src(paths.scripts.src)
-		.pipe(eslint())
-		.pipe(eslint.format())
-		// .pipe(
-		// 	babel({
-		// 		presets: ['@babel/env'],
-		// 	})
-		// )
-		.pipe(gulp.dest(paths.scripts.dist))
-		.pipe(browserSync.stream());
+		.src(paths.scripts.src) // Get JavaScript files
+		.pipe(eslint()) // Lint the JavaScript files
+		.pipe(eslint.format()) // Format the ESLint results
+		.pipe(gulp.dest(paths.scripts.dist)) // Save compiled JS to destination
+		.pipe(browserSync.stream()); // Inject changes into the browser
 }
 
-// Копирование шрифтов
+// Task to copy font files to the destination
 function fontsTask() {
-	return gulp.src(paths.fonts.src).pipe(gulp.dest(paths.fonts.dist));
+	return gulp.src(paths.fonts.src) // Get font files
+		.pipe(gulp.dest(paths.fonts.dist)); // Save fonts to destination
 }
 
-// Оптимизация изображений (с динамическим импортом gulp-imagemin)
+// Task to optimize images
 async function imagesTask() {
-	const imageminInstance = await imagemin();
-	return gulp
-		.src(paths.images.src)
-		.pipe(imageminInstance())
-		.pipe(gulp.dest(paths.images.dist));
+	const imagemin = (await import('gulp-imagemin')).default; // Import imagemin
+	const imageminGifsicle = (await import('imagemin-gifsicle')).default; // Import Gifsicle plugin
+	const imageminMozjpeg = (await import('imagemin-mozjpeg')).default; // Import Mozjpeg plugin
+	const imageminOptipng = (await import('imagemin-optipng')).default; // Import OptiPNG plugin
+	const imageminSvgo = (await import('imagemin-svgo')).default; // Import SVGO plugin
+
+	return gulp.src(paths.images.src) // Get image files
+		.pipe(imagemin([ // Optimize images with specified plugins
+			imageminGifsicle({ interlaced: true }),
+			imageminMozjpeg({ quality: 75, progressive: true }),
+			imageminOptipng({ optimizationLevel: 5 }),
+			imageminSvgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
+		.pipe(gulp.dest(paths.images.dist)); // Save optimized images to destination
 }
 
-// Оптимизация картинок (папка pic)
+// Task to optimize additional images (in the 'pic' folder)
 async function picTask() {
-	const imageminInstance = await imagemin();
-	return gulp
-		.src(paths.pic.src)
-		.pipe(imageminInstance())
-		.pipe(gulp.dest(paths.pic.dist));
+	const imagemin = (await import('gulp-imagemin')).default; // Import imagemin
+	const imageminGifsicle = (await import('imagemin-gifsicle')).default; // Import Gifsicle plugin
+	const imageminMozjpeg = (await import('imagemin-mozjpeg')).default; // Import Mozjpeg plugin
+	const imageminOptipng = (await import('imagemin-optipng')).default; // Import OptiPNG plugin
+	const imageminSvgo = (await import('imagemin-svgo')).default; // Import SVGO plugin
+
+	return gulp.src(paths.pic.src) // Get additional image files
+		.pipe(imagemin([ // Optimize images with specified plugins
+			imageminGifsicle({ interlaced: true }),
+			imageminMozjpeg({ quality: 75, progressive: true }),
+			imageminOptipng({ optimizationLevel: 5 }),
+			imageminSvgo({
+				plugins: [
+					{ removeViewBox: true },
+					{ cleanupIDs: false }
+				]
+			})
+		]))
+		.pipe(gulp.dest(paths.pic.dist)); // Save optimized additional images to destination
 }
 
-// Запуск сервера и отслеживание изменений
+// Task to start the server and watch for changes
 function watchTask() {
-	browserSync.init({
+	browserSync.init({ // Initialize BrowserSync
 		server: {
-			baseDir: './dist',
+			baseDir: './dist', // Set base directory for the server
 		},
 	});
 
+	// Watch for changes in the specified files and run the corresponding tasks
 	gulp.watch(paths.styles.src, sassTask);
 	gulp.watch(paths.pug.src, pugTask);
 	gulp.watch(paths.scripts.src, jsTask);
@@ -120,8 +134,8 @@ function watchTask() {
 	gulp.watch(paths.fonts.src, fontsTask);
 }
 
-// Определение задач
-exports.default = gulp.series(
-	gulp.parallel(pugTask, sassTask, jsTask, fontsTask, imagesTask, picTask),
-	watchTask
+// Define the default tasks
+exports.default = gulp.series( // Run tasks in series
+	gulp.parallel(pugTask, sassTask, jsTask, fontsTask, imagesTask, picTask), // Run tasks in parallel
+	watchTask // Start watching for changes
 );
--- src/js/functions.js
+++ src/js/functions.js
@@ -1,12 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-	initSidebarBtn();
+	initSlider();
+	initImageMove();
 });
 
-function initSidebarBtn() {
-	const resizeBtn = document.querySelector('.resize');
-	const content = document.querySelector('.wrapper');
-	resizeBtn.addEventListener('click', function(e) {
-		e.preventDefault();
-		content.classList.toggle('sb-expand');
+function initSlider() {
+	const colorOptions = document.querySelectorAll('.color-options input');
+	const productImage = document.querySelector('.product-image');
+	const colorNameSpan = document.querySelector('.color-name');
+
+	colorOptions.forEach((option) => {
+		if (option.checked) {
+			const selectedColor = option.value;
+			productImage.src = `pic/sweater-${selectedColor}.webp`;
+			colorNameSpan.textContent = `Color: ${selectedColor.charAt(0).toUpperCase() + selectedColor.slice(1)}`; 
+		}
+	});
+
+	colorOptions.forEach((option) => {
+		option.addEventListener('click', () => {
+			const selectedColor = option.value;
+			productImage.src = `pic/sweater-${selectedColor}.webp`;
+			colorNameSpan.textContent = `Color: ${selectedColor.charAt(0).toUpperCase() + selectedColor.slice(1)}`;
+		});
+	});
+}
+
+function initImageMove() {
+	const productImage = document.querySelector('.product-image');
+	const parentContainer = document.querySelector('.container');
+	parentContainer.addEventListener('mousemove', (event) => {
+		const rect = parentContainer.getBoundingClientRect();
+		const x = event.clientX - rect.left; // Позиция X курсора относительно родителя
+		const y = event.clientY - rect.top;  // Позиция Y курсора относительно родителя
+		
+		// Центр родительского контейнера
+		const centerX = rect.width / 2;
+		const centerY = rect.height / 2;
+		
+		// Смещение изображения на основе расстояния от центра
+		const offsetX = (x - centerX) * -0.1; // умножение на -0.1 для эффекта "отталкивания"
+		const offsetY = (y - centerY) * -0.1;
+	
+		// Применяем смещение к изображению
+		productImage.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
 	});
 }
